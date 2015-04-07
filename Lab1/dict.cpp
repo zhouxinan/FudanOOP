a4392fbe9386eafd777af2176a41fd776b427bfe
zhouxinan <zhouxinan@qq.com>
2015-04-08 01:03:51 +0800

    initial implementation

--- Lab1/dict.cpp
+++ Lab1/dict.cpp
@@ -1,24 +1,56 @@
 #include "dict.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 bool Dict::add(const word &w){
-	// Your code here
-	return false;
+	bool replace_old_value = false;
+	for (int i = 0; i < size; i++){
+		if (dict[i].key == w.key){
+			dict[i].value = w.value;
+			replace_old_value = true;
+			break;
+		}
+	}
+	if (!replace_old_value){
+		if (size >= 5000) return false;
+		dict[size] = w;
+		size++;
+	}
+	return true;
 }
 
 string Dict::search(const string &key){
-	// Your code here
+	for (int i = 0; i < size; i++){
+		if (dict[i].key == key)
+			return dict[i].value;
+	}
 	return "";
 }
 
 string Dict::remove(const string &key){
-	// Your code here
+	for (int i = 0; i < size; i++){
+		if (dict[i].key == key){
+			string value_of_key = dict[i].value;
+			size--;
+			for (int j = i; j < size; j++)
+				dict[j] = dict[j+1];
+			dict[size].key = "";
+			dict[size].value = "";
+			return value_of_key;
+		}
+	}
 	return "";
 }
 
 string Dict::update(const word &w){
-	// Your code here
+	for (int i = 0; i < size; i++){
+		if (dict[i].key == w.key){
+			string previous_value = dict[i].value;
+			dict[i].value = w.value;
+			return previous_value;
+		}
+	}	
 	return "";
 }
 
